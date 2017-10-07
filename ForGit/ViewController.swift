5260f1256530e9072db5f1931f9297b08300e8f2
xcode <xcode@c1r009n03.amm.vsu.ru>
2017-10-07 15:56:32 +0300

    first commit

---
+++ ForGit/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  ForGit
+//
+//  Created by xcode on 07.10.17.
+//  Copyright © 2017 VSU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var name = "Mark"
+    var surname = ""
+    var num = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        num = 123
+        surname = "Lukin"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
